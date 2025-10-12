61050281a8ecb5f22566b5c982d562c7d09efc87
NatalioMaciel <mac24026@byui.edu>
2025-10-11 22:14:11 -0300

    make the HTML structure, basic CSS and fecth the data from the API

---
+++ scripts/utils.js
@@ -0,0 +1,37 @@
+const favorites_key = 'mealscope_favorites';
+
+export function saveFavorite(recipe) {
+    const favorites = loadFavorites();
+    if (!favorites.find(r => r.idMeal === recipe.idMeal)) {
+        favorites.push(recipe);
+        localStorage.setItem(favorites_key, JSON.stringify(favorites));
+    }
+}
+
+export function loadFavorites() {
+    const data = localStorage.getItem(favorites_key);
+    return data ? JSON.parse(data) : [];
+}
+
+export function removeFavorite(idMeal) {
+    let favorites = loadFavorites();
+    favorites = favorites.filter(r => r.idMeal !== idMeal);
+    localStorage.setItem(favorites_key, JSON.stringify(favorites));
+}
+
+export function clearFavorites() {
+    localStorage.removeItem(favorites_key);
+}
+
+
+
+export function recipeCardTemplate(recipe) {
+    return `
+    <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
+    <h3>${recipe.strMeal}</h3>
+    <div class="card-actions">
+      <button class="view-btn">View</button>
+      <button class="fav-btn">⭐</button>
+    </div>
+  `;
+}
